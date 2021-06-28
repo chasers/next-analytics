965111064b89b3675106cf93bfedb8bf1931cad4
chasers <chase@logflare.app>
2021-06-28 09:44:31 -0700

    Test new pino-logflare

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import logger from '../../logger/logger'
+
 export default (req, res) => {
+  logger.info("Responding...")
+
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import logger from '../logger/logger'
 
 export default function Home() {
+  logger.info("Returning...")
+
   return (
+
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
